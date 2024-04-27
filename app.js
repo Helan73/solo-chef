6e3f1e3de929e9e8dba2f2c5b01ba01d5f400ea5
Helan73 <helansharmila73@gmail.com>
2024-04-27 13:19:45 +0530

    commit all

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/User');
+const User = require('./server/models/User.js');
 
 const app=express();
 const port=process.env.PORT || 3000;
@@ -16,6 +16,10 @@ require('dotenv').config();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 app.use(expressLayouts);
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(cookieParser('SoloChefSecure'));
 app.use(session({
@@ -28,7 +32,7 @@ app.use(fileUpload());
 
 app.use(passport.initialize());
 app.use(passport.session());
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./server/routes/authRoutes.js');
 app.use('/auth', authRoutes);
 
 app.set('layout', './layouts/main');
